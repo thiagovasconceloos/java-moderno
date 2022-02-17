edc37d565ce4134c401c0ad566516b87af60c58b
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-17 15:40:16 -0300

    Finalizando o curso

--- src/java8/DataAPI.java
+++ src/java8/DataAPI.java
@@ -1,10 +1,31 @@
 package java8;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class DataAPI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LocalDate hoje = LocalDate.now();
+		
+		System.out.println(hoje);
+		
+		
+		LocalDate aniversario = LocalDate.of(2022,Month.AUGUST,27);
+		//diferença entre meses
+		int meses = aniversario.getMonthValue() - hoje.getMonthValue();
 
+		System.out.println(meses);
+		
+	    //diferença entre dias 
+		Period periodo = Period.between(hoje, aniversario);
+		System.out.println(periodo.getDays());
+		
+		
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String dataFormatada = formatter.format(hoje);
+		System.out.println(dataFormatada);
 	}
 
 }
